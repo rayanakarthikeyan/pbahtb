5ec96fd91c5c486b91e0e9dd24159577374e46e3
Rayana Karthikeyan <rayanakarthikeyan@gmail.com>
2025-10-22 15:20:50 +0530

    Update analyzer.c

--- analyzer.c
+++ analyzer.c
@@ -6,26 +6,32 @@
 #define TABLE_SIZE 100
 #define MAX_KEY_LEN 50
 
-// Node structure for separate chaining
+// Node structure for Separate Chaining
 typedef struct Node {
     char key[MAX_KEY_LEN];
     struct Node *next;
 } Node;
 
-Node *hashTable[TABLE_SIZE];
-
 // Hash function
 unsigned int hashFunction(const char *key) {
     unsigned int hash = 0;
-    while (*key)
+    while (*key) {
         hash = (hash * 31 + *key++) % TABLE_SIZE;
+    }
     return hash;
 }
 
 // ------------------- LINEAR PROBING -------------------
 int linearProbingInsert(char keys[][MAX_KEY_LEN], int n, double loadFactor) {
     int size = (int)(n / loadFactor);
+    if (size <= 0) size = 1;
+
     char **table = (char **)calloc(size, sizeof(char *));
+    if (!table) {
+        printf("Memory allocation failed!\n");
+        return -1;
+    }
+
     int collisions = 0;
 
     for (int i = 0; i < n; i++) {
@@ -37,14 +43,25 @@ int linearProbingInsert(char keys[][MAX_KEY_LEN], int n, double loadFactor) {
         table[index] = strdup(keys[i]);
     }
 
+    for (int i = 0; i < size; i++) {
+        free(table[i]);
+    }
     free(table);
+
     return collisions;
 }
 
 // ------------------- QUADRATIC PROBING -------------------
 int quadraticProbingInsert(char keys[][MAX_KEY_LEN], int n, double loadFactor) {
     int size = (int)(n / loadFactor);
+    if (size <= 0) size = 1;
+
     char **table = (char **)calloc(size, sizeof(char *));
+    if (!table) {
+        printf("Memory allocation failed!\n");
+        return -1;
+    }
+
     int collisions = 0;
 
     for (int i = 0; i < n; i++) {
@@ -58,22 +75,37 @@ int quadraticProbingInsert(char keys[][MAX_KEY_LEN], int n, double loadFactor) {
         table[(index + j * j) % size] = strdup(keys[i]);
     }
 
+    for (int i = 0; i < size; i++) {
+        free(table[i]);
+    }
     free(table);
+
     return collisions;
 }
 
 // ------------------- SEPARATE CHAINING -------------------
 int separateChainingInsert(char keys[][MAX_KEY_LEN], int n, double loadFactor) {
     int size = (int)(n / loadFactor);
+    if (size <= 0) size = 1;
+
     Node **table = (Node **)calloc(size, sizeof(Node *));
+    if (!table) {
+        printf("Memory allocation failed!\n");
+        return -1;
+    }
+
     int collisions = 0;
 
     for (int i = 0; i < n; i++) {
         unsigned int index = hashFunction(keys[i]) % size;
-
-        if (table[index] != NULL) collisions++;
+        if (table[index] != NULL)
+            collisions++;
 
         Node *newNode = (Node *)malloc(sizeof(Node));
+        if (!newNode) {
+            printf("Memory allocation failed!\n");
+            return -1;
+        }
         strcpy(newNode->key, keys[i]);
         newNode->next = table[index];
         table[index] = newNode;
@@ -113,27 +145,35 @@ int main(int argc, char *argv[]) {
     char keys[1000][MAX_KEY_LEN];
     int count = 0;
 
-    while (fscanf(file, "%s", keys[count]) != EOF && count < 1000)
+    while (fscanf(file, "%s", keys[count]) != EOF && count < 1000) {
         count++;
+    }
     fclose(file);
+
+    if (count == 0) {
+        printf("Error: Input file is empty.\n");
+        return 1;
+    }
 
     int collisions = 0;
 
-    if (strcmp(technique, "Linear_Probing") == 0)
+    if (strcmp(technique, "Linear_Probing") == 0) {
         collisions = linearProbingInsert(keys, count, loadFactor);
-    else if (strcmp(technique, "Quadratic_Probing") == 0)
+    } else if (strcmp(technique, "Quadratic_Probing") == 0) {
         collisions = quadraticProbingInsert(keys, count, loadFactor);
-    else if (strcmp(technique, "Separate_Chaining") == 0)
+    } else if (strcmp(technique, "Separate_Chaining") == 0) {
         collisions = separateChainingInsert(keys, count, loadFactor);
-    else {
+    } else {
         printf("Invalid Technique: %s\n", technique);
         return 1;
     }
 
-    printf("Technique: %s\n", technique);
-    printf("Load Factor: %.2f\n", loadFactor);
-    printf("Total Inserts: %d\n", count);
-    printf("Collisions: %d\n", collisions);
+    if (collisions >= 0) {
+        printf("Technique: %s\n", technique);
+        printf("Load Factor: %.2f\n", loadFactor);
+        printf("Total Inserts: %d\n", count);
+        printf("Collisions: %d\n", collisions);
+    }
 
     return 0;
 }
